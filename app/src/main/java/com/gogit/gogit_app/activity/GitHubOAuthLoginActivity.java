12f1f39d0d571598f4e3f9eb20928c5e6ca45ce7
hyeg <hyeg0121@naver.com>
2023-09-05 22:28:23 +0900

    github oauth 로그인 페이지 연결

--- app/src/main/java/com/gogit/gogit_app/activity/GitHubOAuthLoginActivity.java
+++ app/src/main/java/com/gogit/gogit_app/activity/GitHubOAuthLoginActivity.java
@@ -1,27 +1,37 @@
 package com.gogit.gogit_app.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static kotlinx.coroutines.BuildersKt.launch;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.browser.customtabs.CustomTabsIntent;
+
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Toast;
 
 import com.gogit.gogit_app.R;
+import com.gogit.gogit_app.client.MyWebViewClient;
 import com.gogit.gogit_app.util.Util;
 
 public class GitHubOAuthLoginActivity extends AppCompatActivity {
+
+    String url = "http://10.0.2.2:8080/login/github";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_git_hub_oauth_login);
 
-        Uri.Builder builder = Uri.parse(Util.GITHUB_AUTHORIZE_URL).buildUpon()
-                .appendQueryParameter("client_id", Util.GITHUB_CLIENT_ID)
-                .appendQueryParameter("redirect_uri", Util.GITHUB_CALLBACK_URL)
-                .appendQueryParameter("scope", "user");
+        WebView webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webView.setWebViewClient(new MyWebViewClient());
+        webSettings.setJavaScriptEnabled(true);
 
-        Intent intent = new Intent(Intent.ACTION_VIEW, builder.build());
-        startActivity(intent);
+        webView.loadUrl(url);
     }
 }
