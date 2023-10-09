c35aa855fd23fe569367a4b5ef1394a66ee09be0
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-09 21:45:33 +0900

    버튼을 클릭했을 때 웹뷰로 넘어감

--- app/src/main/java/com/gogit/gogit_app/activity/LoginActivity.java
+++ app/src/main/java/com/gogit/gogit_app/activity/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.gogit.gogit_app.R;
 
@@ -12,9 +13,12 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.login);
 
-        Intent intent = new Intent(this, GitHubOAuthLoginActivity.class);
-        startActivity(intent);
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(e -> {
+            Intent intent = new Intent(this, GitHubOAuthLoginActivity.class);
+            startActivity(intent);
+        });
     }
 }
