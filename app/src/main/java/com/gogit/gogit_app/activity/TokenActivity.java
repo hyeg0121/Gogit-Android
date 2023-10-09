a86870d90d4b66ff553e3e9c58fbb2294eb3555a
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-09 21:42:03 +0900

    토큰 입력 액티비티 생성

---
+++ app/src/main/java/com/gogit/gogit_app/activity/TokenActivity.java
@@ -0,0 +1,18 @@
+package com.gogit.gogit_app.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gogit.gogit_app.R;
+
+public class TokenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_token);
+
+
+    }
+}
