e219a057ba7075847cdbb7bd91486c44e3c54aef
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-09 22:48:21 +0900

    유저 프로필 액티비티

---
+++ app/src/main/java/com/gogit/gogit_app/activity/MemberMainActivity.java
@@ -0,0 +1,16 @@
+package com.gogit.gogit_app.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gogit.gogit_app.R;
+
+public class MemberMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_user);
+    }
+}
