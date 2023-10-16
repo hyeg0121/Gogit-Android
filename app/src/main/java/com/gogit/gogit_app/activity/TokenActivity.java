ff875e0d343c3e70a3348971820019896dc56f70
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-16 17:58:36 +0900

    토큰 입력받기

--- app/src/main/java/com/gogit/gogit_app/activity/TokenActivity.java
+++ app/src/main/java/com/gogit/gogit_app/activity/TokenActivity.java
@@ -3,7 +3,9 @@ package com.gogit.gogit_app.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -14,42 +16,57 @@ import com.gogit.gogit_app.R;
 import org.kohsuke.github.GitHub;
 import org.kohsuke.github.GitHubBuilder;
 
-
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class TokenActivity extends AppCompatActivity {
 
+    String myToken = "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";
+
     @SuppressLint("CheckResult")
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_token);
+
+        // TODO: 토큰이 있으면 바로 메인으로 가기
 
         Button submitButton = findViewById(R.id.submit_button);
         EditText toeknEditText = findViewById(R.id.token_editText);
 
 
         submitButton.setOnClickListener(e -> {
+            // 입력된 토큰 가져오기
             String token = toeknEditText.getText().toString();
-            Log.d("my tag", token);
+            // TODO: 토큰 유효성 검사하기
+            // TODO: 빈 칸일 때 리턴
+            SharedPreferences sharedPreferences = getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE);
 
-            Observable.fromCallable(() -> {
-                GitHub gitHub = new GitHubBuilder().withOAuthToken(myToken).build();
-                return gitHub;
-            })
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(
-                            gitHub -> {
-                                Intent intent = new Intent(TokenActivity.this, MemberMainActivity.class);
-                                startActivity(intent);
-                            },
-                            error -> {
-                                Log.d("my tag", "error");
-                            }
-                    );
+            // 저장된 토큰 가져오기
+            String storedToken = sharedPreferences.getString("access_token", null);
+
+            if (storedToken == null) {
+                // 저장된 토큰이 존재하지 않으면 저장
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("access_token", myToken);
+                editor.apply();
+            }
+
+            Intent intent = new Intent(TokenActivity.this, MemberMainActivity.class);
+            startActivity(intent);
+
+//            Observable.fromCallable(() -> {
+//                GitHub gitHub = new GitHubBuilder().withOAuthToken(myToken).build();
+//                return gitHub;
+//            })
+//                    .subscribeOn(Schedulers.io())
+//                    .observeOn(AndroidSchedulers.mainThread())
+//                    .subscribe(
+//                            gitHub -> {
+//                            },
+//                            error -> {
+//                            }
+//                    );
 
 
         });
