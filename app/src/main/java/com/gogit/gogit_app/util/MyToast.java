0860118121cbd6227e862661c9abc37780ce53ef
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-27 23:08:08 +0900

    토스트 메세지 유틸 클래스 생성

---
+++ app/src/main/java/com/gogit/gogit_app/util/MyToast.java
@@ -0,0 +1,12 @@
+package com.gogit.gogit_app.util;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class MyToast {
+
+    public static void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+}
+
