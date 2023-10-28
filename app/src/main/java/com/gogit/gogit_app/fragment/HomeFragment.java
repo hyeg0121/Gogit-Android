f574de1dc4e0e1929d75f2b10679c0beac8855b5
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-28 22:40:28 +0900

    Home 프래그먼트 생성

---
+++ app/src/main/java/com/gogit/gogit_app/fragment/HomeFragment.java
@@ -0,0 +1,25 @@
+package com.gogit.gogit_app.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gogit.gogit_app.R;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.home, container, false);
+
+        return view;
+    }
+}
