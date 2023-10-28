05abcfa27eea7b9de539558133a74c55dfb908a5
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-28 23:23:57 +0900

    리포지토리 생성 프래그먼트

---
+++ app/src/main/java/com/gogit/gogit_app/fragment/CreateRepoFragment.java
@@ -0,0 +1,21 @@
+package com.gogit.gogit_app.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gogit.gogit_app.R;
+
+
+public class CreateRepoFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_create_repo, container, false);
+        return view;
+    }
+}
