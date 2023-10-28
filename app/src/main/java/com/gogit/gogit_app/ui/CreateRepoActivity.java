659e61b75dba68300f98088b004c41f97eb67f5b
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-28 22:38:23 +0900

    리포지토리 생성 액티비티

---
+++ app/src/main/java/com/gogit/gogit_app/ui/CreateRepoActivity.java
@@ -0,0 +1,15 @@
+package com.gogit.gogit_app.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gogit.gogit_app.R;
+
+public class CreateRepoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
