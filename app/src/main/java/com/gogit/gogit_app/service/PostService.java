f2e31aa8dbd3a4a4d79d301cc74bd6a8b6b8718d
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-31 13:43:03 +0900

    post service

---
+++ app/src/main/java/com/gogit/gogit_app/service/PostService.java
@@ -0,0 +1,14 @@
+package com.gogit.gogit_app.service;
+
+import com.gogit.gogit_app.dto.AddPostRequest;
+import com.gogit.gogit_app.dto.Post;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface PostService {
+    @POST("api/posts")
+    Call<Post> createdPost(@Body AddPostRequest addPostRequest);
+}
