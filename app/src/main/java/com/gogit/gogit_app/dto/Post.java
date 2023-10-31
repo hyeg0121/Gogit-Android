b2ba8d71fdb8040b6333a1419d78e2748d4516a3
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-31 23:29:31 +0900

    업로드 날짜 필드 추가

--- app/src/main/java/com/gogit/gogit_app/dto/Post.java
+++ app/src/main/java/com/gogit/gogit_app/dto/Post.java
@@ -1,5 +1,6 @@
 package com.gogit.gogit_app.dto;
 
+import java.util.Date;
 import java.util.List;
 
 public class Post {
@@ -8,13 +9,19 @@ public class Post {
     private final String contents;
     private final List<Comment> comment;
     private final List<Member> likedMembers;
+    private final Date createdAt;
 
-    public Post(Long id, Member writer, String contents, List<Comment> comment, List<Member> likedMembers) {
+    public Post(Long id, Member writer, String contents, List<Comment> comment, List<Member> likedMembers, Date createdAt) {
         this.id = id;
         this.writer = writer;
         this.contents = contents;
         this.comment = comment;
         this.likedMembers = likedMembers;
+        this.createdAt = createdAt;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
     }
 
     public Long getId() {
