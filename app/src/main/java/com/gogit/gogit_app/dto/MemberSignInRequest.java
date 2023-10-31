bcf1ddb3ba3be21af96869ba609299efd21415f9
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-31 23:29:00 +0900

    멤버 필드 추가

--- app/src/main/java/com/gogit/gogit_app/dto/MemberSignInRequest.java
+++ app/src/main/java/com/gogit/gogit_app/dto/MemberSignInRequest.java
@@ -3,10 +3,14 @@ package com.gogit.gogit_app.dto;
 public class MemberSignInRequest {
     private final String githubId;
     private final String githubToken;
+    private final String avatarUrl;
+    private final String htmlUrl;
 
-    public MemberSignInRequest(String githubId, String githubToken) {
+    public MemberSignInRequest(String githubId, String githubToken, String avatarUrl, String htmlUrl) {
         this.githubId = githubId;
         this.githubToken = githubToken;
+        this.avatarUrl = avatarUrl;
+        this.htmlUrl = htmlUrl;
     }
 
 }
