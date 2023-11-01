a42dd26a2191287f01256de2bc31449d937af5d6
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-01 23:58:47 +0900

    상수 이름 변경

--- app/src/main/java/com/gogit/gogit_app/client/RetrofitClient.java
+++ app/src/main/java/com/gogit/gogit_app/client/RetrofitClient.java
@@ -5,12 +5,12 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit;
-    private static final String GITHUB_BASE_URL = "http://10.0.2.2:8080";
+    private static final String BASE_URL = "http://10.0.2.2:8080";
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl(GITHUB_BASE_URL)
+                    .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
