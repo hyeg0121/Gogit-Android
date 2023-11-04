53e9127a1a2e2afd9c4e15415087972a6d9c210a
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-04 13:18:49 +0900

    네트워크 불안정 토스트 메세지 추가

--- app/src/main/java/com/gogit/gogit_app/util/MyToast.java
+++ app/src/main/java/com/gogit/gogit_app/util/MyToast.java
@@ -8,5 +8,10 @@ public class MyToast {
     public static void showToast(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
+
+    public static void showNetworkErrorToast(Context context) {
+        Toast.makeText(context, "네트워크가 불안정합니다.", Toast.LENGTH_SHORT).show();
+    }
+
 }
 
