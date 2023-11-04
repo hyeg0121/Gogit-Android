d469d38deb909bc8095dcbb9865746f85b7712ce
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-04 13:32:49 +0900

    모든 포스트 가져오는 api

--- app/src/main/java/com/gogit/gogit_app/service/PostService.java
+++ app/src/main/java/com/gogit/gogit_app/service/PostService.java
@@ -3,6 +3,8 @@ package com.gogit.gogit_app.service;
 import com.gogit.gogit_app.dto.AddPostRequest;
 import com.gogit.gogit_app.dto.Post;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
@@ -11,4 +13,7 @@ import retrofit2.http.POST;
 public interface PostService {
     @POST("/posts")
     Call<Post> createdPost(@Body AddPostRequest addPostRequest);
+
+    @GET("/posts")
+    Call<List<Post>> getAllPosts();
 }
