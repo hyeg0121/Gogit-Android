f0aeafec01bc3f41ba3db54380f4788ebbd9a3c2
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-04 19:06:43 +0900

    리포지토리 검색결과 클래스

---
+++ app/src/main/java/com/gogit/gogit_app/model/SearchedRepo.java
@@ -0,0 +1,39 @@
+package com.gogit.gogit_app.model;
+
+import java.util.List;
+
+public class SearchedRepo {
+    private Integer total_count;
+    private Boolean incomplete_results;
+    private List<Repository> items;
+
+    public SearchedRepo(Integer total_count, Boolean incomplete_results, List<Repository> items) {
+        this.total_count = total_count;
+        this.incomplete_results = incomplete_results;
+        this.items = items;
+    }
+
+    public Integer getTotal_count() {
+        return total_count;
+    }
+
+    public void setTotal_count(Integer total_count) {
+        this.total_count = total_count;
+    }
+
+    public Boolean getIncomplete_results() {
+        return incomplete_results;
+    }
+
+    public void setIncomplete_results(Boolean incomplete_results) {
+        this.incomplete_results = incomplete_results;
+    }
+
+    public List<Repository> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Repository> items) {
+        this.items = items;
+    }
+}
