9b9e5de4fd2931b0238622bd0c13e7c9c9b37a6e
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-07 20:13:47 +0900

    이슈 메뉴를 클릭하면 이슈 창으로 이동

---
+++ app/src/main/java/com/gogit/gogit_app/fragment/IssuesFragment.java
@@ -0,0 +1,26 @@
+package com.gogit.gogit_app.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gogit.gogit_app.R;
+
+public class IssuesFragment extends Fragment {
+
+    public IssuesFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.issues, container, false);
+
+
+        return view;
+    }
+}
