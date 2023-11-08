8d2277dd5ee7b4edc69a3561c2545a80ab60dee0
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-08 10:15:30 +0900

    이슈 model

---
+++ app/src/main/java/com/gogit/gogit_app/model/Issue.java
@@ -0,0 +1,67 @@
+package com.gogit.gogit_app.model;
+
+public class Issue {
+    private Long number;
+    private String title;
+    private String state;
+    private GithubUser user;
+    private Repository repository;
+    private String body;
+
+    public Issue(Long number, String title, String state, GithubUser user, Repository repository, String body) {
+        this.number = number;
+        this.title = title;
+        this.state = state;
+        this.user = user;
+        this.repository = repository;
+        this.body = body;
+    }
+
+    public Long getNumber() {
+        return number;
+    }
+
+    public void setNumber(Long number) {
+        this.number = number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public GithubUser getUser() {
+        return user;
+    }
+
+    public void setUser(GithubUser user) {
+        this.user = user;
+    }
+
+    public Repository getRepository() {
+        return repository;
+    }
+
+    public void setRepository(Repository repository) {
+        this.repository = repository;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
