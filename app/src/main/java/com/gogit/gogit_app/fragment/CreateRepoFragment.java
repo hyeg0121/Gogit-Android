20126f2f969823a05732acf0d831f2c8c4bbca7e
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-12 15:31:26 +0900

    코드 스타일 수정

--- app/src/main/java/com/gogit/gogit_app/fragment/CreateRepoFragment.java
+++ app/src/main/java/com/gogit/gogit_app/fragment/CreateRepoFragment.java
@@ -30,20 +30,21 @@ import retrofit2.Retrofit;
 public class CreateRepoFragment extends Fragment {
 
     GithubService githubService;
+    Retrofit githubRetrofit = GithubRetrofitClient.getRetrofitInstance();
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.create_repository, container, false);
 
-        SessionManager sessionManager = new SessionManager(getContext());
-        String userId = sessionManager.getUserId();
-        String token = sessionManager.getToken();
-
         EditText nameEditText = view.findViewById(R.id.name_editText);
         EditText descEditText = view.findViewById(R.id.desc_editText);
         Button uploadButton = view.findViewById(R.id.upload_button);
 
-        Retrofit githubRetrofit = GithubRetrofitClient.getRetrofitInstance();
+        // 유저 정보
+        SessionManager sessionManager = new SessionManager(getContext());
+
         githubService = githubRetrofit.create(GithubService.class);
 
         uploadButton.setOnClickListener(e -> {
@@ -51,27 +52,33 @@ public class CreateRepoFragment extends Fragment {
             String name = nameEditText.getText().toString();
             String description = descEditText.getText().toString();
 
-            AddRepositoryRequest addRepositoryRequest = new AddRepositoryRequest(name, description);
 
-            Call<Map<String, Object>> createRepoCall = githubService.createRepo(
-                    "Bearer " + token,
-                    addRepositoryRequest);
-
-            createRepoCall.enqueue(new Callback<Map<String, Object>>() {
-                @Override
-                public void onResponse(Call<Map<String, Object>> call, Response<Map<String, Object>> response) {
-                    // TODO: 리포지토리 목록 나열할 페이지 만들기
-                    FragmentHelper.replaceFragment(getFragmentManager(), R.id.containers, new MyPageFragment());
-                }
-
-                @Override
-                public void onFailure(Call<Map<String, Object>> call, Throwable t) {
-                    MyToast.showToast(getContext(), "네트워크가 불안정합니다.");
-                }
-            });
-
+            createRepo(sessionManager.getToken(), name, description);
         });
 
         return view;
     }
+
+    public void createRepo(String token, String name, String description) {
+        AddRepositoryRequest addRepositoryRequest = new AddRepositoryRequest(name, description);
+
+        Call<Map<String, Object>> createRepoCall = githubService.createRepo(
+                "Bearer " + token,
+                addRepositoryRequest);
+
+        createRepoCall.enqueue(new Callback<Map<String, Object>>() {
+            @Override
+            public void onResponse(Call<Map<String, Object>> call, Response<Map<String, Object>> response) {
+                // TODO: 리포지토리 목록 나열할 페이지 만들기
+                MyToast.showToast(getContext(), "리포지토리가 추가되었습니다.");
+                FragmentHelper.replaceFragment(getFragmentManager(), R.id.containers, new MyPageFragment());
+            }
+
+            @Override
+            public void onFailure(Call<Map<String, Object>> call, Throwable t) {
+                MyToast.showToast(getContext(), "네트워크가 불안정합니다.");
+            }
+        });
+    }
+
 }
