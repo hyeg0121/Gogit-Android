df0f778838d8903f53d46b591c34ec4c358968e5
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-12 23:34:16 +0900

    커밋 모델

--- app/src/main/java/com/gogit/gogit_app/model/github/commit/Commit.java
+++ app/src/main/java/com/gogit/gogit_app/model/github/commit/Commit.java
@@ -1,5 +1,21 @@
 package com.gogit.gogit_app.model.github.commit;
 
-public class Commit {
+import java.util.Map;
 
+public class Commit {
+    private Map<String, String> author;
+    private String message;
+
+    public Commit(Map<String, String> author, String message) {
+        this.author = author;
+        this.message = message;
+    }
+
+    public Map<String, String> getAuthor() {
+        return author;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
